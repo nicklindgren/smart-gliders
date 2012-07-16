a5ea46738ee1e353672abc4e1bcf969de0b8b2be
Nicolas Lindgren <nicolas.lindgren@gmail.com>
2012-07-16 01:34:57 -0500

    Clarified some variable names; also, the golly bruteForceSeach.py script now works on m*n search patterns as well as m*m search patterns.

--- golly/bruteForceSearch.py
+++ golly/bruteForceSearch.py
@@ -8,8 +8,8 @@ def getIndices(x):
     return [start,end]
 
 def bruteForceSearch(searchPatt):
-    p = len(searchPatt)
-    q = len(searchPatt[0])
+    cols = len(searchPatt[0])
+    rows = len(searchPatt)
     matches = list()
 
     wd = golly.getwidth()
@@ -22,26 +22,26 @@ def bruteForceSearch(searchPatt):
 
     # Only search the candidate matrix cells which can contain
     # the matrix we're searching for
-    candidates = [(x,y) for x in range(startWd,endWd-p+2) for y in range(startHt,endHt-q+2)]
+    candidates = [(x,y) for x in range(startWd,endWd-cols+2) for y in range(startHt,endHt-rows+2)]
     for (iCol, iRow) in candidates:
         jCol = 0
         jRow = 0 
         while(golly.getcell(iCol + jCol,iRow + jRow) == searchPatt[jRow][jCol]):
-            if (jCol == p-1) and (jRow == q-1):
+            if (jCol == cols-1) and (jRow == rows-1):
                 print '        Match = ({0},{1})'.format(iCol,iRow)
                 matches.append((iCol,iRow))
                 break
             jCol = jCol + 1
-            if (jCol > p - 1):
+            if (jCol > cols - 1):
                 jCol = 0
                 jRow = jRow + 1
-                if (jRow > q - 1):
+                if (jRow > rows - 1):
                     print '        Break = ({0},{1})'.format(iCol,iRow)
                     break ## End of search space, and no match found
     return [matches, candidates]
 
 #golly.note('Vals = {0}  {1}'.format(golly.getcell(0,0),golly.getcell(-2,0)))
-searchPattern = golly.getstring("Define a pattern to search for (enter as a Python list)", "[[1,0,0],[0,1,0],[0,0,1]]", "Enter A Pattern")
+searchPattern = golly.getstring("Define a pattern to search for (enter as a Python list)", "[[1,0],[1,1],[0,0]]", "Enter A Pattern")
 [results, candidates] = bruteForceSearch(eval(searchPattern))
 golly.note('Total Matches: {0}\nTotal Candidate Cells:{1}\n{2}'.format(
         len(results), len(candidates), results))
